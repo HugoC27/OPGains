4464692580011b4d74a84e676d5df5b418a657d5
HugoC27 <cigehn.hugo@gmail.com>
2023-12-08 23:45:01 +0100

    Colored visualization screens

--- app/src/main/java/com/example/opgains/VisualizationBackScreen.kt
+++ app/src/main/java/com/example/opgains/VisualizationBackScreen.kt
@@ -15,15 +15,74 @@ import com.example.opgains.ui.theme.OPGainsTheme
 
 @Composable
 fun VisualizationBackScreen(navController: NavController, modifier: Modifier = Modifier) {
-    val backMuscleTemplate = painterResource(R.drawable.back_template)
+    var scoreBackCalves = 1
+    var scoreBackHamstrings = 3
+    var scoreBackGlutes = 5
+    var scoreBackLowerback = 2
+    var scoreBackLats = 4
+    var scoreBackTrapezius = 3
+    var scoreBackNeck = 1
+    var scoreBackShoulders = 5
+    var scoreBackTricep = 1
+    var scoreBackForearm = 4
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
             .fillMaxSize()
     ) {
         Image(
-            painter = backMuscleTemplate,
+            painter = painterResource(R.drawable.back_template),
             contentDescription = "back_template",
+        )
+        Image(
+            painter = painterResource(R.drawable.back_calves),
+            contentDescription = "back_calves",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackCalves)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_hamstring),
+            contentDescription = "back_hamstring",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackHamstrings)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_glutes),
+            contentDescription = "back_glutes",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackGlutes)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_lowerback),
+            contentDescription = "back_lowerback",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackLowerback)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_lats),
+            contentDescription = "back_lats",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackLats)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_trapezius),
+            contentDescription = "back_trapezius",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackTrapezius)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_neck),
+            contentDescription = "back_neck",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackNeck)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_shoulders),
+            contentDescription = "back_shoulders",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackShoulders)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_tricep),
+            contentDescription = "back_triceps",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackTricep)
+        )
+        Image(
+            painter = painterResource(R.drawable.back_forearm),
+            contentDescription = "back_forearm",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreBackForearm)
         )
     }
     Box(
--- app/src/main/java/com/example/opgains/VisualizationFrontScreen.kt
+++ app/src/main/java/com/example/opgains/VisualizationFrontScreen.kt
@@ -26,18 +26,96 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.opgains.ui.theme.OPGainsTheme
+import androidx.compose.ui.graphics.ColorFilter
 
 @Composable
 fun VisualizationFrontScreen(navController: NavController, modifier: Modifier = Modifier) {
-    val frontMuscleTemplate = painterResource(R.drawable.front_template)
+    var scoreFrontCalf = 2
+    var scoreFrontThigh = 4
+    var scoreFrontHipAbductor = 1
+    var scoreFrontAbs = 4
+    var scoreFrontOblique = 5
+    var scoreFrontLats = 4
+    var scoreFrontChest = 3
+    var scoreFrontTraps = 2
+    var scoreFrontNeck = 1
+    var scoreFrontShoulder = 5
+    var scoreFrontTriceps = 2
+    var scoreFrontBiceps = 4
+    var scoreFrontForearms = 3
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
             .fillMaxSize()
     ) {
         Image(
-            painter = frontMuscleTemplate,
+            painter = painterResource(R.drawable.front_template2),
             contentDescription = "front_template",
+        )
+        Image(
+            painter = painterResource(R.drawable.front_calf),
+            contentDescription = "front_calf",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontCalf)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_thigh),
+            contentDescription = "front_thigh",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontThigh)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_hip_abductor),
+            contentDescription = "front_hip_abductor",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontHipAbductor)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_abs),
+            contentDescription = "front_abs",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontAbs)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_obliques),
+            contentDescription = "front_oblique",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontOblique)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_lats),
+            contentDescription = "front_lats",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontLats)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_chest),
+            contentDescription = "front_chest",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontChest)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_traps),
+            contentDescription = "front_lats",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontTraps)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_neck),
+            contentDescription = "front_neck",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontNeck)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_shoulders),
+            contentDescription = "front_shoulder",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontShoulder)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_tricep),
+            contentDescription = "front_tricep",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontTriceps)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_bicep),
+            contentDescription = "front_biceps",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontBiceps)
+        )
+        Image(
+            painter = painterResource(R.drawable.front_forearms),
+            contentDescription = "front_forearms",
+            colorFilter = ColorVariableChecker(muscleVariable = scoreFrontForearms)
         )
     }
     Box(
@@ -51,6 +129,19 @@ fun VisualizationFrontScreen(navController: NavController, modifier: Modifier = 
             visButtonColor2 = (Color(0xFF94A150))
         )
     }
+}
+
+@Composable
+fun ColorVariableChecker(muscleVariable: Int): ColorFilter? {
+    val color: Color? = when (muscleVariable) {
+        5 -> Color(0xFFFF3B3B)
+        4 -> Color(0xFFFF5353)
+        3 -> Color(0xFFFF6969)
+        2 -> Color(0xFFFF7E7E)
+        1 -> Color(0xFFFF9696)
+        else -> null
+    }
+    return color?.let { ColorFilter.tint(it) }
 }
 
 @Composable
