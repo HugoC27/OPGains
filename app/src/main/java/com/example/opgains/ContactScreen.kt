fa4728d4a3c704da404ea30f0f74257a4b002a25
Hugo Lennartsson <hugo.lennartsson@outlook.com>
2023-12-09 17:28:09 +0100

    Fixade ContactPreview

--- app/src/main/java/com/example/opgains/ContactScreen.kt
+++ app/src/main/java/com/example/opgains/ContactScreen.kt
@@ -1,9 +1,13 @@
 package com.example.opgains
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Phone
@@ -13,23 +17,62 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.opgains.ui.theme.OPGainsTheme
 
 @Composable
 fun ContactScreen(navController: NavController, modifier: Modifier = Modifier) {
+    val camoBackground = painterResource(R.drawable.camo_background)
+    val whiteBox = painterResource(R.drawable.whitecontact)
+    val appLogo = painterResource(R.drawable.opgainsround)
     Column {
         Box(
             contentAlignment = Alignment.Center,
             modifier = modifier
                 .fillMaxSize()
-                .background(Color.Cyan)
+                .background(Color(0xFFA4B25C))
         ) {
-            Text("Kontaktinformation")
+            Image(
+                painter = camoBackground,
+                contentDescription = "Background",
+                contentScale = ContentScale.Crop,
+                alpha = 0.6F,
+                modifier = modifier
+                    .fillMaxSize()
+            )
+            Image(
+                painter = whiteBox,
+                contentDescription = "frame",
+                contentScale = ContentScale.Crop,
+                modifier = modifier
+                    .height(350.dp)
+                    .width(280.dp)
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Image(
+                    painter = appLogo,
+                    contentDescription = null,
+                    contentScale = ContentScale.Fit,
+                    modifier = modifier
+                        .height(200.dp)
+                        .width(200.dp)
+                        .padding(bottom = 15.dp)
+                )
+                Text("opgains@gmail.com")
+                Text("+46 123 456 789")
+
+            }
         }
     }
+
+
     Box(
         contentAlignment = Alignment.BottomStart,
         modifier = modifier
